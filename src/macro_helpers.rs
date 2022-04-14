757c9846178f82cbe9c56d871b3ea34d9ca470a4
Jay Jackson <jj@jj.is-a.dev>
2022-04-13 23:17:57 -0400

    reset

--- src/macro_helpers.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! with_dollar_sign {
-  ($($body:tt)*) => {
-      macro_rules! __with_dollar_sign { $($body)* }
-      __with_dollar_sign!($);
-  }
-}
-
-pub use with_dollar_sign;
